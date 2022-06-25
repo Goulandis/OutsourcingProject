5db601384e101d3a1f9e18f8fdad69f4434a2bc1
austin.li <austin.li@cloudminds.com>
2022-06-25 12:31:13 +0800

    增加英文语言，增加摄像头视角切换，场景优化，增加动态更改分辨率和热力图相机图标

---
+++ Nike/Source/Nike/GamePlay/NikeGameInstanceBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NikeGameInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIKE_API UNikeGameInstanceBase : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
