3131f4bcbeff70b3ebdff91386889f51272ba1b0
Goulandis <1612160117@qq.com>
2022-09-27 10:04:59 +0800

    V1.3

--- FlightSimulator/Source/FlightSimulator/Common/CoordinateConversionFunLib.cpp
+++ FlightSimulator/Source/FlightSimulator/Common/CoordinateConversionFunLib.cpp
@@ -8,11 +8,13 @@ FVector UCoordinateConversionFunLib::WGS84ToCartesian(float lat, float lon, floa
     float EarthRedius = 5000000.0f;
     //H指的就是距离海平面的大地高，N+H也就是当前测点与地心的矢径
     float f = 1.0f / 298.257223563f;
-    float b = EarthRedius * (1 - f); 
+    float b = EarthRedius * (1 - f);
+    lat = lat * (-1);
     float e = FMath::Sqrt(EarthRedius * EarthRedius - b * b) / EarthRedius;
-    float N = EarthRedius / FMath::Sqrt(1 - e * e * FMath::Sin(lat * PI / 180) * FMath::Sin(lat * PI / 180));
-    float X = (N + h) * FMath::Cos(lat * PI / 180) * FMath::Cos(lon * PI / 180);
-    float Y = (N + h) * FMath::Cos(lat * PI / 180) * FMath::Sin(lon * PI / 180);
-    float Z = (N * (1 - (e * e)) + h) * FMath::Sin(lat * PI / 180);
-    return FVector(X,Y,Z);
+    float N = EarthRedius / FMath::Sqrt(1 - e * e * FMath::Sin(lon * PI / 180) * FMath::Sin(lon * PI / 180));
+    float X = (N + h) * FMath::Cos(lon * PI / 180) * FMath::Cos(lat * PI / 180);
+    float Y = (N + h) * FMath::Cos(lon * PI / 180) * FMath::Sin(lat * PI / 180);
+    float Z = (N * (1 - (e * e)) + h) * FMath::Sin(lon * PI / 180);
+    return FVector(X, Y, Z);
 }
+
