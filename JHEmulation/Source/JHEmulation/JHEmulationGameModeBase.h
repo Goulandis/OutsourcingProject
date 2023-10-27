8a9de28adcd17d162465114ebe37944b3510749f
Goulandis <1612160117@qq.com>
2023-10-27 22:48:32 +0800

    完成rtsp视频流测试最小demo

---
+++ JHEmulation/Source/JHEmulation/JHEmulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JHEmulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JHEMULATION_API AJHEmulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
