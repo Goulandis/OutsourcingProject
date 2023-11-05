78272292dd0293e7232b0dc9f95054d5ac7b323c
Goulandis <1612160117@qq.com>
2023-11-05 11:41:14 +0800

    文件调整

--- JHEmulation/Source/JHEmulation/FileFunLib.cpp
+++ JHEmulation/Source/JHEmulation/FileFunLib.cpp
@@ -9,9 +9,8 @@ FString UFileFunLib::ReadFile(const FString& Path,bool& Rel)
 {
 	Rel = false;
 	FString RelStr;
-	std::ifstream Ifs;
-	std::string PathStr = TCHAR_TO_UTF8(&Path);
-	Ifs.open(PathStr);
+	const std::string PathStr = TCHAR_TO_UTF8(*Path);
+	std::ifstream Ifs(PathStr.c_str());
 	if(Ifs.is_open())
 	{
 		UE_LOG(LogTemp,Error,TEXT("Start Read"));
@@ -25,6 +24,10 @@ FString UFileFunLib::ReadFile(const FString& Path,bool& Rel)
 		Rel = true;
 		RelStr = FString(UTF8_TO_TCHAR(Content.c_str()));
 		UE_LOG(LogTemp,Error,TEXT("Content:%s"),*RelStr);
+	}
+	else
+	{
+		UE_LOG(LogTemp,Error,TEXT("Failed to open file : %s"),*Path);
 	}
 	Ifs.close();
 	return RelStr;
